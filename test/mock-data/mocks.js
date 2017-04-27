a15462ebe6136b80f928f67c8b478316af03fb5f
Alex Bennett <alexandergraybennett@gmail.com>
2017-04-27 06:42:51 -0600

    adds tests

---
+++ test/mock-data/mocks.js
@@ -0,0 +1,9 @@
+let mockData = {
+  title: 'New Products - Acme',
+  name: 'Acme FPWC-2 fake plastic watering can, 2 gallon, green',
+  image: 'http://placekitten.com/g/300/300',
+  incorrectImageURL: '8gbefwg87wefg897we',
+  description: 'A green plastic watering can for a fake Chinese rubber plant in the fake plastic '
+}
+
+module.exports = mockData;
---
+++ test/product.spec.js
@@ -0,0 +1,85 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var expect = require('chai').expect;
+var app = require('../app.js');
+var config = require('../config/config.json');
+var mockData = require('./mock-data/mocks.js');
+chai.use(chaiHttp);
+
+describe('Loop List API Products Test Suite', done => {
+
+  let id = 12345;
+
+  it('has a product endpoint that takes an ID and returns 200 on valid request', done => {
+    chai.request(`${ config.DEV.endpoint }${ id }`)
+      .get('/')
+      .end((err, response) => {
+        expect(response.status).to.equal(200)
+        done();
+      });
+  });
+
+  it('it also should return a payload', done => {
+    chai.request(`${ config.DEV.endpoint }${ id }`)
+      .get('/')
+      .end((err, response) => {
+        expect(response.body).to.exist
+        done();
+      });
+  });
+
+  it('payload should contain document title', done => {
+    chai.request(`${ config.DEV.endpoint }${ id }`)
+      .get('/')
+      .end((err, response) => {
+        console.log('mockData');
+        expect(response.body.title).to.equal(mockData.title)
+        done();
+      });
+  });
+
+  it('payload should contain product name', done => {
+    chai.request(`${ config.DEV.endpoint }${ id }`)
+      .get('/')
+      .end((err, response) => {
+        expect(response.body.name).to.equal(mockData.name)
+        done();
+      });
+  });
+
+  it('payload should contain product description', done => {
+    chai.request(`${ config.DEV.endpoint }${ id }`)
+      .get('/')
+      .end((err, response) => {
+        expect(response.body.description).to.equal(mockData.description)
+        done();
+      });
+  });
+
+  it('payload should contain product main image url', done => {
+    chai.request(`${ config.DEV.endpoint }${ id }`)
+      .get('/')
+      .end((err, response) => {
+        expect(response.body.image).to.equal(mockData.image);
+        done();
+      });
+  });
+
+  it('payload should contain correct product main image url', done => {
+    chai.request(`${ config.DEV.endpoint }${ id }`)
+      .get('/')
+      .end((err, response) => {
+        expect(response.body.image).to.not.equal(mockData.incorrectImageURL);
+        done();
+      });
+  });
+
+  it('endpoint should return a 404 on abad request', done => {
+    chai.request(`http://localhost:3000/products/90823thmf4`)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
