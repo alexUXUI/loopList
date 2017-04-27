e0e2c1dd85a4e85a6c78da54a385c63c063e91a4
Alex Bennett <alexandergraybennett@gmail.com>
2017-04-27 08:01:01 -0600

    deletes log

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var API = require('../API/products.js')
 
 router.get('/:id', (req, res, next) => {
-  console.log(API);
   let { id } = req.params;
   API.parser(id).then(data => res.json(data));
 });
